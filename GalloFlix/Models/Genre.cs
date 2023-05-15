434c039432c3622059ad9d09ded3a055185923be
Rafaela Torelli e Julia Delfino <rafaelaalessandratorelli@gmail.com>
2023-05-15 08:39:26 -0300

    Adição das propriedades dos itens do Genre.cs e Movie.cs

--- GalloFlix/Models/Genre.cs
+++ GalloFlix/Models/Genre.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GalloFlix.Models;
 
 public class Genre
 {
+    [Key] // Notaçãozinha pra chave primária - entende-se que esse Id é chave primária (NOTAÇÃO VEM SEMPRE ANTES DA PROPRIEDADE)
     public byte Id { get; set; }
+
+    [Required] // Requerido - Not Null no banco; torna o nome obrigatório
+    [StringLength(30)] // Tamanho máximo da propriedade
     public string Name { get; set; } 
 }
